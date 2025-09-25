31bf140859805a392df906c618af2b70f1f66f93
Maks <Maksym.Bodnar.RI.2025@lpnu.ua>
2025-09-25 23:53:56 +0200

    added final

---
+++ Lab_02/Lab_02.cpp
@@ -0,0 +1,31 @@
+﻿// Lab_02.cpp
+// < Боднар Максим >
+// Лабораторна робота № 2.
+// Лінійні програми.
+// Варіант 2
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double Pi = 4 * atan(1.); // число пі
+	
+	double a; // вхідний параметр
+	double x; // вхідний параметр
+	double z1; // результат обчислення 1-го виразу
+	double z2; // результат обчислення 2-го виразу
+	
+	cout << "a = "; cin >> a;
+	cout << "x = "; cin >> x;
+	
+	z1 = cos(a) + sin(a) + cos(3 * a) + sin(3 * a);
+	z2 = 2 * sqrt(2) * cos(a) * cos((Pi/4) + (2 * a));
+	
+	cout << endl;
+	cout << "z1 = " << z1 << endl;
+	cout << "z2 = " << z2 << endl;
+	
+	cin.get();
+	return 0;
+}
+
